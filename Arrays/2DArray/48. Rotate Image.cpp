784bd00923d6f24b047b39c952bc058e6b73937c
AkankshaVPatil <akanksha.v.patil@gmail.com>
2024-07-04 23:01:38 +0200

    Create 48. Rotate Image.cpp

---
+++ Arrays/2DArray/48. Rotate Image.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/rotate-image/description/
+//48. Rotate Image
+// TC- O(n*n), SC- O(1)
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for(auto &row:matrix)
+        {
+            reverse(row.begin(), row.end());
+        }
+    }
+};
