3a4ea02b65a166a92b78480172d4d515c00bd26c
AkankshaVPatil <akanksha.v.patil@gmail.com>
2024-07-04 23:04:46 +0200

    Update 48. Rotate Image.cpp

--- Arrays/2DArray/48. Rotate Image.cpp
+++ Arrays/2DArray/48. Rotate Image.cpp
@@ -20,3 +20,26 @@ public:
         }
     }
 };
+
+
+
+//------ Approach - 2--------------
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i; j < n; j++)// only chang in line
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for(auto &row:matrix)
+        {
+            reverse(row.begin(), row.end());
+        }
+    }
+};
