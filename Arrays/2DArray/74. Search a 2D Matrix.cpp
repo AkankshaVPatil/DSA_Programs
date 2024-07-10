41533f42ab8f658dda1aa6c9828696bd0153ae4e
AkankshaVPatil <akanksha.v.patil@gmail.com>
2024-07-10 14:14:19 +0200

    Create 74. Search a 2D Matrix.cpp

---
+++ Arrays/2DArray/74. Search a 2D Matrix.cpp
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/search-a-2d-matrix/description/
+//74. Search a 2D Matrix
+
+//----------Approach-I
+// TC - O(log(m*n)), SC - O(N)
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int l = 0, r = m * n - 1;
+
+        while(l <= r)// 1 < 1
+        {
+            int mid = (l + r)/2;//1
+
+            if(matrix[mid/n][mid%n] > target)
+                r = mid - 1;
+            else if(matrix[mid/n][mid%n] < target)
+                l = mid + 1;
+            else
+                return true;
+        }
+        return false;
+    }
+};
