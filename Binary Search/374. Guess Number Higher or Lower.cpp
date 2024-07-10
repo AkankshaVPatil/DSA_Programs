633ed0dd278f8192d959b8c9d5ee0d961a507b74
AkankshaVPatil <akanksha.v.patil@gmail.com>
2024-07-10 20:56:13 +0200

    Create 374. Guess Number Higher or Lower.cpp

---
+++ Binary Search/374. Guess Number Higher or Lower.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/guess-number-higher-or-lower/description/
+//374. Guess Number Higher or Lower
+// TC - O(logn), SC - O(n)
+class Solution {
+public:
+    int guessNumber(int n) {
+        long l = 1;
+        long r = n;
+
+        while(l <= r)// 6 <= 7
+        {
+            long mid = (l + r)/2; //6
+            int gs = guess(mid);
+            if(gs == 1)
+                l = mid + 1;
+            else if(gs == -1)
+                r = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+};
