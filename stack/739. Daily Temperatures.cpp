bb5990e54b71a07f0404261eeed7f7dc018063e6
AkankshaVPatil <akanksha.v.patil@gmail.com>
2024-07-11 19:52:57 +0200

    Create 739. Daily Temperatures.cpp

---
+++ stack/739. Daily Temperatures.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/daily-temperatures/
+//739. Daily Temperatures
+// TC - O(n), SC - O(n)
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int>st;
+        vector<int> ans(temperatures.size(), 0);
+        for(int i = 0; i < temperatures.size(); i++)
+        {
+            while(!st.empty() && temperatures[i] > temperatures[st.top()])
+            {
+                ans[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
