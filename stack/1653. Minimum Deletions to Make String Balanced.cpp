50a89e20425d3394848ba6f90308ca41356c6b6a
AkankshaVPatil <akanksha.v.patil@gmail.com>
2024-07-30 11:15:24 +0200

    Update 1653. Minimum Deletions to Make String Balanced.cpp

--- stack/1653. Minimum Deletions to Make String Balanced.cpp
+++ stack/1653. Minimum Deletions to Make String Balanced.cpp
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/description
 //1653. Minimum Deletions to Make String Balanced.cpp
+//------------Approach-I----------
 // TC - O(n), SC - O(n)
 class Solution {
 public:
@@ -17,5 +18,30 @@ public:
                 st.push(c);
         }
         return cntr;
+    }
+};
+
+//------------Approach-II----------
+// TC - O(n), SC - O(1)
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int a_count = 0;
+        for(auto c:s)
+        {
+            if(c == 'a')
+                a_count++;
+        }
+
+        int count = INT_MAX, b_count = 0;
+        for(auto c:s)
+        {
+            if(c == 'a')
+                a_count--;
+            count = min(count, a_count + b_count);
+            if(c == 'b')
+                b_count++;
+        }
+        return count;
     }
 };
