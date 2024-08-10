4cca5ad941c65d34330b51d1fe5810abd2548ec2
AkankshaVPatil <akanksha.v.patil@gmail.com>
2024-08-10 22:14:47 +0200

    Update 876. Middle of the Linked List.cpp

--- LinkedList/876. Middle of the Linked List.cpp
+++ LinkedList/876. Middle of the Linked List.cpp
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/middle-of-the-linked-list/description/
 //876. Middle of the Linked List
+//---------Approach-I------
 // TC - O(n), SC - O(1)
 class Solution {
 public:
@@ -11,5 +12,33 @@ public:
             slow = slow->next;
         }
         return slow;
+    }
+};
+
+//---------Approach-II------
+// TC - O(n), SC - O(1)
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        int cntr = 0;
+        ListNode* curr = head;
+        while(curr != NULL)
+        {
+            cntr++;
+            curr = curr->next;
+        }
+
+        int mid = cntr/2 + 1;
+        cntr = 0;
+        curr = head;
+        while(curr != NULL)
+        {
+            cntr++;
+            if(cntr == mid)
+                return curr;
+            curr = curr->next;
+        }
+        return NULL;
     }
 };
