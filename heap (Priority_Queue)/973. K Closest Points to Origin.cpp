9ad2f69874bbbedf2b63b145fcdf759c282e1e0f
AkankshaVPatil <akanksha.v.patil@gmail.com>
2024-08-13 19:21:26 +0200

    Update 973. K Closest Points to Origin.cpp

--- heap (Priority_Queue)/973. K Closest Points to Origin.cpp
+++ heap (Priority_Queue)/973. K Closest Points to Origin.cpp
@@ -1,6 +1,6 @@
 //https://leetcode.com/problems/k-closest-points-to-origin/
 //973. K Closest Points to Origin
-
+//--------Approach-I-------------
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
@@ -14,6 +14,26 @@ public:
         }
         vector<vector<int>> vec;
         while(!pq.empty())
+        {
+            vec.push_back(pq.top().second);
+            pq.pop();
+        }
+        return vec;
+    }
+};
+
+//--------Approach-II---------------
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int, vector<int>>, vector<pair<int, vector<int>>>, greater<pair<int, vector<int>>>>pq;
+        for(auto p:points)
+        {
+            int val = (p[0]*p[0])+(p[1]*p[1]);            
+            pq.push({val, p});
+        }
+        vector<vector<int>> vec;
+        while(!pq.empty() && k--)
         {
             vec.push_back(pq.top().second);
             pq.pop();
