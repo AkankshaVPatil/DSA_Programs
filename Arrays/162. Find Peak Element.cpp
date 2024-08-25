502738894c6ce52903c47fae8314099a07129d86
AkankshaVPatil <akanksha.v.patil@gmail.com>
2024-08-25 11:19:09 +0200

    Create 162. Find Peak Element.cpp

---
+++ Arrays/162. Find Peak Element.cpp
@@ -0,0 +1,38 @@
+//https://leetcode.com/problems/find-peak-element/
+//162. Find Peak Element
+// TC - O(logn), SC - O(1)
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int lo = 0;
+        int hi = nums.size()-1;
+        while(lo < hi)
+        {
+            int mid = lo + (hi - lo)/2;
+            if(nums[mid] < nums[mid+1])
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        return lo;
+    }
+};
+
+// TC - O(logn), SC - O(logn)
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int lo, int hi)
+    {
+        if(lo == hi) return lo;
+        int mid = lo + (hi - lo)/2;
+        if(nums[mid] < nums[mid+1])
+            return binarySearch(nums, mid + 1, hi);
+
+        return binarySearch(nums, lo, mid);
+    }
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        return binarySearch(nums, 0, n-1);
+    }
+};
